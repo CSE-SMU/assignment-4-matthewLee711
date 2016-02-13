340da00c63492ed7f56564e2268b685305208657
Matthew Lee <matthew.lee711@gmail.com>
2016-02-12 22:11:59 -0600

    fixed ordering

--- js/todoAngular.js
+++ js/todoAngular.js
@@ -18,19 +18,29 @@ var dateModule = angular.module('datepickerBasicUsage',
 
 angular.module('ToDo', []).
   // Controller added to prevent functions from global scope
-  controller('todoController',['$scope',function($scope) {
+  controller('todoController',['$scope', '$filter', function($scope, $filter) {
+    var orderBy = $filter('orderBy');
     $scope.todos = [
-      {'title':'Build a todo app', 'description':'work', 'date': '2016-02-18', 'done':true},
-      {'title':'Welcome', 'description':'Try out my to do list!', 'date':'2016-02-18', 'done': false}
+      {'title':'Build a to do app', 'description':'work', 'date': '2016-02-18', 'done':true},
+      {'title':'Welcome', 'description':'Try out my to do list!', 'date':'2016-02-18', 'done': false},
+      {'title':'aa', 'description':'Try out my to do list!', 'date':'2016-02-18', 'done': false}
     ];
-
+    
+    $scope.order = function(predicate) {
+      $scope.predicate = predicate;
+      $scope.todos = orderBy($scope.todos, predicate);
+    };
+    $scope.order('date');
 
     //Function which adds items to array
     $scope.addTodo = function(){
       $scope.todos.push({'title':$scope.newtodo,'description':$scope.newdescription,
         'date':$scope.newdate, 'done':false})
       $scope.newtodo = ''
+      $scope.newdescription = ''
+      $scope.newdate = ''
     }
+    $scope.order('title');
   //Function which removes items from array
     $scope.clearCompleted = function(){
       $scope.todos = $scope.todos.filter(function(item) {
